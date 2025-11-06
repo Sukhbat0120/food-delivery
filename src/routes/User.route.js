116641d347cf44b7ee3a76b239876816c2b4588f
25LP2243 25LP2243 <25LP2243@G303-CC0130.local>
2025-11-06 11:41:44 +0800

    user delete,update

--- src/routes/User.route.js
+++ src/routes/User.route.js
@@ -1,6 +1,16 @@
 import { Router } from "express";
-import { login, signUp } from "../controlled/user.controller.js";
+import {
+  deleteUser,
+  login,
+  signUp,
+  updateUser,
+} from "../controlled/user.controller.js";
+import { verifyToken } from "../middleware/auth.js";
 
 export const userRouter = Router();
 
-userRouter.post("/signup", signUp).get("/sign-in", login);
+userRouter
+  .post("/signup", signUp)
+  .get("/sign-in", login, verifyToken)
+  .delete("/delete", deleteUser)
+  .patch("/update", updateUser);
